89f8de1f6b4c22e193f5563354948e60139a2ec6
Leann Ukena <leannukena@Leanns-MacBook-Pro.local>
2017-03-16 18:06:52 -0400

    Add styles to weather page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,22 +3,26 @@ var React = require('react');
 var About = (props) => {
       return (
         <div>
-          <h2 className="text-center">All About Weather</h2>
+          <h1 className="text-center">About</h1>
+          <p>This weather app is built on React and Open Weather Map. I created
+          this app for the amazing Complete React Web App Developer Course.</p>
+
+          <h4 className="text-center">All About Weather</h4>
           <ul>
             <li>
-              <a href="https://en.wikipedia.org/wiki/Weather">Weather Facts</a> - Visit
+              <a href="https://en.wikipedia.org/wiki/Weather" target="_blank">Weather Facts</a> - Visit
               Wikipedia Weather for weather facts.
             </li>
             <li>
-              <a href="https://weather.com/">Weather News</a> - Visit The
+              <a href="https://weather.com/" target="_blank">Weather News</a> - Visit The
               Weather Channel for weather news.
             </li>
             <li>
-              <a href="http://www.noaa.gov/">Weather Science</a> - Visit NOAA - The
+              <a href="http://www.noaa.gov/" target="_blank">Weather Science</a> - Visit NOAA - The
               National Oceanic & Atmospheric Administration for weather science.
             </li>
             <li>
-              <a href="https://www.sercc.com/wxtrivia.html">Weather Trivia</a> - Visit
+              <a href="https://www.sercc.com/wxtrivia.html" target="_blank">Weather Trivia</a> - Visit
               the Southeast Regional Climate Center for weather trivia.
             </li>
           </ul>
