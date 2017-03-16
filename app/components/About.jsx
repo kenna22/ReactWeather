fc0a1994d6cf217befcf74a67a6e362d6ca85c7c
Leann Ukena <leannukena@Leanns-MacBook-Pro.local>
2017-03-16 15:52:12 -0400

    Add content & style about

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,25 @@ var React = require('react');
 var About = (props) => {
       return (
         <div>
-          <h3>About</h3>
-          <p>Welcome to about page!</p>
+          <h2 className="text-center">All About Weather</h2>
+          <ul>
+            <li>
+              <a href="https://en.wikipedia.org/wiki/Weather">Weather Facts</a> - Visit
+              Wikipedia Weather for weather facts.
+            </li>
+            <li>
+              <a href="https://weather.com/">Weather News</a> - Visit The
+              Weather Channel for weather news.
+            </li>
+            <li>
+              <a href="http://www.noaa.gov/">Weather Science</a> - Visit NOAA - The
+              National Oceanic & Atmospheric Administration for weather science.
+            </li>
+            <li>
+              <a href="https://www.sercc.com/wxtrivia.html">Weather Trivia</a> - Visit
+              the Southeast Regional Climate Center for weather trivia.
+            </li>
+          </ul>
         </div>
       )
 };
